19d7f7fbf54c2a8d3f7b40c02259fa95569134ff
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-05-23 04:11:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Register from "./pages/Register";
 import { UserData } from "./context/User";
 import Loading from "./components/Loading";
 import Admin from "./pages/Admin";
-import PlayList from "./pages/PlayList";
+import PlayList from "./pages/PlayList"; 
 import Album from "./pages/Album";
 
 const App = () => {
